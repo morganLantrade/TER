5d2b099dde81563ba089e2eba9e35201bdf45aab
Enguerran-Couderc <enguerrancouderc@gmail.com>
2023-05-24 18:31:54 +0200

    ajout join cartesien, update generation db avec double, update sort merge join pour double

--- Codes/generator.py
+++ Codes/generator.py
@@ -3,12 +3,15 @@ import pandas as pd
 
 
 
-def generate_db(N,selectivity):
+def generate_db(N,selectivity,double=False):
     relation=int(N*selectivity)
     R=[]
     S=[]
-    RY=[i for i in range(1,N+1)]
-    random.shuffle(RY)
+    if double:
+        RY=[random.randint(1,9) for _ in range(N)]
+    else:
+        RY=[i for i in range(1,N+1)]
+        random.shuffle(RY)
     SY=random.sample(RY,relation)
     L=[]
     L=[i for i in range(-N+1,1)]
@@ -29,28 +32,49 @@ def sort_merge(R,S):
     R=R.reset_index(drop=True)
     S=S.reset_index(drop=True)
     T=[]
-    iS=0
     iR=0
+    iS=0
     while iS<N and iR<N :
         if R['Y'].get(iR)==S['Y'].get(iS):
-            T.append((R['X'].get(iR),S['Z'].get(iS)))
+            T.append((R['X'].get(iR),R['Y'].get(iR),S['Z'].get(iS)))
             iS+=1
-            iR+=1
         elif R['Y'].get(iR)>S['Y'].get(iS):
             iS+=1
         else:
+            iS-=1
+            while R['Y'].get(iR)==S['Y'].get(iS):
+                iS-=1
+            iS+=1
             iR+=1
-    return pd.DataFrame(T,columns=['X','Z'])
+    return pd.DataFrame(T,columns=['X','Y','Z'])
     
+def cartesian_product(R,S,cond="R.Y=Z.Y"):
+    if cond=="R.Y=Z.Y":
+        n=len(R)
+        m=len(S)
+        T=[]
+        for i in range(n):
+            for j in range(m):
+                if R['Y'].get(i)==S['Y'].get(j):
+                    T.append((R['X'].get(i),R['Y'].get(i),S['Z'].get(j)))
+    return pd.DataFrame(T,columns=['X','Y','Z'])
+
 
 if __name__ == '__main__':
     N=10
     selectivity=0.8
-    R,S=generate_db(N,selectivity)
-    print(R)
+    R,S=generate_db(N,selectivity,double=True)
+
+    print(R.sort_values(by=['Y']))
+    print()
+    print(S.sort_values(by=['Y']))
+    print()
+    print(sort_merge(R,S))
+
+    '''print(R)
     print()
     print(S)
     print()
-    print(sort_merge(R,S))
+    print(cartesian_product(R,S))'''
 
     #print(R.join(S.set_index('Y'), on='Y',how='inner'))
