33ff7e046092b76784abec9f56a501e5dde2b1af
Lantrade Morgan <morgan.lantrade@gmail.com>
2023-06-07 17:08:48 +0200

    hybrid hash join cas : table R rentre dans la memoire

--- Codes/generator.py
+++ Codes/generator.py
@@ -272,63 +272,87 @@ def cartesian_product_index(R,S,selectivity,memory,size_of_tuple,size_of_page,si
     
     return read_build_th,written_build_th,read_probe_th,write_probe_th,read_build_exp,written_build_exp,read,written
 
+def hash_join(R,S,selectivity,memory,size_of_tuple,size_of_page):
+    ##Theoric##
+    R_pages,_= number_of_pages(R,size_of_tuple,size_of_page)
+    S_pages,_=number_of_pages(S,size_of_tuple,size_of_page)
+    tuples_per_page= size_of_page//size_of_tuple
 
-
-def hash1(x):
-    return x%3
-
-def fnv_1a_hash_numeric(data):
-    '''Fonction de hachage fnv_1a (source : wikipedia/chat GPT)
-    Se renseigner dessus sur les perfomances collisions/mémoires'''
-    FNV_OFFSET_BASIS = 0x811C9DC5
-    FNV_PRIME = 0x01000193
-    hash_value = FNV_OFFSET_BASIS
-
-    # Convertir le nombre en octets
-    byte_data = bytes(str(data), 'utf-8')
-
-    for byte in byte_data:
-        hash_value ^= byte
-        hash_value *= FNV_PRIME
-
-    return hash_value & 0xFFFFFFFF
-
-
-
-def hash_join(R,S,hash_function=fnv_1a_hash_numeric):
-    '''Renvoi un inner join des tables R et S en utilisant un algorithme de hachage simple
-    collisions gérées par chainage'''
-    H=dict()
-    n=len(R)
-    m=len(S)
-    T=[]
-    read0=0
-    written0=0
-    #Remplissage de la table de hachage de R
-    for i in range(n):
-        x,y=R['X'].get(i),R['Y'].get(i)
-        read0+=2
-        key=hash_function(y)
-        if key in H:
-            H[key].append((x,y))
-        else:
-            H[key]=[(x,y)]
-        written0+=1
+    #Build
+    read_build_th = R_pages
+    written_build_th = R_pages #nombre pages index
     
+    #Probe
+    read_probe_th = S_pages
+    write_probe_th = math.ceil(math.ceil(len(R)*selectivity)/tuples_per_page)
+
+    ##Experiment##
+
+    #Build
     written=0
     read=0
-    # Parcours de S
-    for j in range(m):
-        y,z=S['Y'].get(j),S['Z'].get(j)
-        read+=2 # on lit y de S + la clé
-        key=hash_function(y)
-        if key in H: #la clé existe on parcour toute la chaine
+    R=R.values.tolist()
+    S=S.values.tolist()
+    H=dict()
+    T=[]
+    for x,y in R:
+        read+=1
+        key=y%3
+        written+=1
+        if key in H:
+            H[key].append((x,y))
+
+        else:
+            H[key]=[(x,y)]
+
+    read_build_exp=math.ceil(read/tuples_per_page)
+    written_build_exp=math.ceil(written/tuples_per_page)
+    
+    #Probe
+    written=0
+    read=0
+    for y,z in S:
+        read+=1
+        key=y%3
+        if key in H:
             for (Rx,Ry) in H[key]:
-                read+=1 # un autre tuple
-                if y==Ry:
-                    written+=1
+                if Ry==y:
                     T.append((Rx,Ry,z))
-    return pd.DataFrame(T,columns=['X','Y','Z']),read0,written0,read,written
+                    written+=1
+    read_probe_exp=math.ceil(read/tuples_per_page)
+    written_probe_exp=math.ceil(written/tuples_per_page)
+    T=pd.DataFrame(T,columns=['X','Y','Z'])
+    return T,read_build_th,written_build_th,read_probe_th,write_probe_th,read_build_exp,written_build_exp,read_probe_exp,written_probe_exp
+
+
+
+
+
+
+def hybrid_hash_join(R,S,selectivity,memory,size_of_tuple,size_of_page):
+    '''Renvoi une simulation memoire d'un join des tables R et S en utilisant un algorithme de hachage hybride'''
+    
+    ##Theoric##
+    
+    R_pages,_= number_of_pages(R,size_of_tuple,size_of_page)
+    S_pages,_=number_of_pages(S,size_of_tuple,size_of_page)
+    tuples_per_page= size_of_page//size_of_tuple
+    pages_per_partition_R  = math.ceil(R_pages/(memory-1))
+    print(R_pages,S_pages,memory)
+    print("Page par partition de R", pages_per_partition_R)
+    #page par partitions de la plus grande relation S + 1 page pour charger partition R + output
+    assert memory>  pages_per_partition_R+2, f'Memory overflow : les partitions ont des taille de {pages_per_partition} pages pour une mémoire de {memory} pages dont deux sont requises pour le join'
+    
+
+    
+
+    ##Experiment##
+    if R_pages + 2 > memory: #on doit partitionner
+        return None
+    else:
+        return hash_join(R,S,selectivity,memory,size_of_tuple,size_of_page)
+        
+
 
 
 
@@ -408,20 +432,40 @@ def test_sort_merge_join(Rsize,Ssize,selectivity,memory,size_of_tuple,size_of_pa
     print('Lecture :',read_probe_exp,'/ Ecriture :',written_probe_exp)
     print("---")
 
-    
+def test_hybrid_hash_join(Rsize,Ssize,selectivity,memory,size_of_tuple,size_of_page):
+    R,S=generate_db(Rsize,Ssize,selectivity,double=False)
+    print("-"*10)
+    print("Hybrid Hash Join")
+    print("-"*10)
+    T,read_build_th,written_build_th,read_probe_th,write_probe_th,read_build_exp,written_build_exp,read_probe_exp,written_probe_exp=hybrid_hash_join(R,S,selectivity,memory,size_of_tuple,size_of_page)
+    print(T)
+    print("----") 
+    print("Theorique Build")
+    print('Lecture :',read_build_th,'/ Ecriture :',written_build_th)
+    print("----")
+    print("Theorique Probe")
+    print('Lecture :',read_probe_th,'/ Ecriture :',write_probe_th)
+    print("----")
+    print("Experimental Build: \n")
+    print('Lecture :',read_build_exp,'/ Ecriture :',written_build_exp)
+    print("----")
+    print("Experimental Probe: \n")
+    print('Lecture :',read_probe_exp,'/ Ecriture :',written_probe_exp)
+    print("---")
+
 if __name__ == '__main__':
     
-    Rsize=10
-    Ssize=20
+    Rsize=350
+    Ssize=650
     selectivity=1
-    memory=10
+    memory=13
     size_of_page=1024
     size_of_tuple=32
     size_key_index=8
     #test_cartesian_product(Rsize=1000,Ssize=2000,selectivity=0.25,memory=3,size_of_tuple=32,size_of_page=1024)
     #test_cartesian_product_index(Rsize,Ssize,selectivity,memory,size_of_tuple,size_of_page,size_key_index)
-    test_sort_merge_join(Rsize,Ssize,selectivity,memory,size_of_tuple,size_of_page)
-
+    #test_sort_merge_join(Rsize,Ssize,selectivity,memory,size_of_tuple,size_of_page)
+    test_hybrid_hash_join(Rsize,Ssize,selectivity,memory,size_of_tuple,size_of_page)
     '''
     R,S=generate_db(Rsize,Ssize,selectivity,double=False)
     b,c=number_of_pages(R,32,1024)
