9425978b89f892421f0a2435d35c3885b562e015
Lantrade Morgan <morgan.lantrade@gmail.com>
2023-07-06 13:20:33 +0200

    indexation implemente version file et test de recherche + commentaire

--- Codes/index.py
+++ Codes/index.py
@@ -1,35 +1,43 @@
-import random
+#######################################################################
+# source : https://www.geeksforgeeks.org/introduction-of-b-tree-2/
+# modifié pour notre structure de données et le transformer en B+ tree
+########################################################################
+import pandas as pd
+from tools import *
 
-# Create a node
+
 class BTreeNode:
+  '''Noeud de l'arbre'''
   def __init__(self, leaf=False):
     self.leaf = leaf
     self.keys = []
     self.child = []
  
  
-# Tree
 class BTree:
+  '''order correspont au degres de l'arbre, max feuille = order - 1'''
   def __init__(self, order):
     self.root = BTreeNode(True)
     self.order = order
  
-    # Insert node
+  
   def insert(self, k):
+    '''Insertion de k dans l'arbre'''
     root = self.root
+    #arbre plein
     if len(root.keys) == self.order - 1:
       temp = BTreeNode()
       self.root = temp
       temp.child.insert(0, root)
-      
       self.split_child(temp, 0)
       self.insert_non_full(temp, k)
 
     else:
       self.insert_non_full(root, k)
  
-    # Insert nonfull
+
   def insert_non_full(self, x, k):
+    '''Insertion dans un noeud non plein'''
     i = len(x.keys) - 1
     if x.leaf:
       x.keys.append((None, None))
@@ -43,7 +51,6 @@ class BTree:
         i -= 1
       i += 1
       #si le children est plein
-
       if  len(x.child[i].keys) == self.order :
         self.split_child(x, i)
         if k[0] > x.keys[i][0]:
@@ -51,29 +58,29 @@ class BTree:
       #insertion a la fin 
       self.insert_non_full(x.child[i], k)
  
-    # Split the child
   def split_child(self, x, i):
+    '''Debordement, separation du children'''
     y = x.child[i]
     z = BTreeNode(y.leaf)
     x.child.insert(i+1 , z)
     x.keys.insert(i, y.keys[self.order//2 - 1])
-    
+    #si l'enfant de x possede des feuilles
     if y.leaf:
-      z.keys = [y.keys[self.order//2-1]]+y.keys[self.order//2:] #y.keys[self.order//2:]  #
+      z.keys = [y.keys[self.order//2-1]]+y.keys[self.order//2:] 
       y.keys =  y.keys[:self.order//2-1]
     else:
-      z.child =  y.child[self.order//2:]#y.child[self.order//2:]
+      z.child =  y.child[self.order//2:]
       y.child = y.child[:self.order//2]
-      z.keys = y.keys[self.order//2:] #y.keys[self.order//2:]  #
+      z.keys = y.keys[self.order//2:] 
       y.keys =  y.keys[:self.order//2-1]
     
  
-  # Print the tree
+  
   def print_tree(self, x, l=0):
-    print("Level ", l, " ( ", end=":")
+    '''Affiche l'arbre en longueur'''
+    print("Level ", l, " ( ", end=" ")
     for i in x.keys:
       print(i, end=" ")
-
     print(")")
     l += 1
     if len(x.child) > 0:
@@ -81,76 +88,116 @@ class BTree:
         self.print_tree(i, l)
 
   def levels(self,x,dic,l=0):
-    print("Level ", l, " ", len(x.keys),len(x.child), end=":")
-    
-    for i in x.keys:
-      print(i, end=" ")
+    '''Renvoie un dictionnaire ou les cles sont les niveaux et
+     les valeurs sont les différents branche ou feuilles correspondants  '''  
     if l in dic:
       dic[l].append([ k for k in x.keys] )
     else: 
       dic[l]=[[ k for k in x.keys]]
-    print()
     l += 1
     if len(x.child) > 0:
       for i in x.child:
         self.levels(i,dic, l)
     return dic     
  
-  # Search key in the tree
-  def search_key(self, k, x=None):
-    if x is not None:
-      i = 0
-      while i < len(x.keys) and k> x.keys[i][0]:
-        i += 1
+def index_of(dataframe,pageSize):
+    '''Renvoie le dictionnaire levels selon un dataframe et la taille d'une page(en tuple)'''
+    B=BTree(pageSize)
+    L= dataframe.values.tolist()
+    for y,x in L:
+        B.insert((int(y),int(x)))
+    B.print_tree(B.root)
+    return B.levels(B.root,dict())
 
-      if i < len(x.keys) and k == x.keys[i][0]:
-        return (x, i)
-      elif x.leaf:
-        return None
-      else:
-        
-        return self.search_key(k, x.child[i])
-       
-    else:
-      return self.search_key(k, self.root)
-
-
-    
-
- 
- 
-def main():
-  B = BTree(6)
-  L= [ i for i in range(100)]
-  random.shuffle(L)
-  for x in L:
-    B.insert((x,0))
-    if x<20:
-      B.print_tree(B.root)
-  dic=B.levels(B.root,dict())
-  print(dic)
-  cpt_page=1
-  cpt_page_level=0 
-  for level in list(dic.keys())[:-1]:
-    print(level)
-    print("-------------")
-    for v in dic[level]:
-      print("--",cpt_page_level,"--")
-      cpt_page_level+=1 
-      for p in v:
-        print((cpt_page,p[0]))
-        cpt_page+=1
-      
-      print((cpt_page,-1))
-      cpt_page+=1
-  print(level)
-  print("-------------")
-  for v in dic[len(list(dic.keys()))-1]:
-        print("--",cpt_page_level,"--")
+def index_page(dic):
+    '''Retourne une liste de liste correspondant aux differente pas a ecrire avec leur contenu et le nombre de niveaux'''
+    cpt_page=1
+    cpt_page_level=0 
+    pages=[]
+    #Niveau pointeur
+    for level in list(dic.keys())[:-1]:
+        for v in dic[level]:
+            page=[]
+            for p in v:
+                page.append((cpt_page,p[0]))
+                cpt_page+=1
+            page.append((cpt_page,-1))
+            cpt_page+=1
+            pages.append(page)
         cpt_page_level+=1 
+
+    #Niveau feuille
+    for v in dic[len(list(dic.keys()))-1]:
+        page=[]
         for p in v:
-          print(p[1],p[0])
+            page.append((p[1],p[0]))
+        pages.append(page)
+    return pages,cpt_page_level
+
+def index_to_file(folderName,pageSize,dbName):
+    '''Ecrit dans la run foldername_idx, l'index et retourne le nombre de niveaux'''
+    if not os.path.exists('Data/'+folderName+"_idx"):
+        os.makedirs('Data/'+folderName+"_idx")
+    else:
+        #vide le contenu
+        delete_file("I",folderName+"_idx")
+    #metadonnee
+    nbPage=len([f for f in os.listdir("Data/"+folderName) if dbName in f])
+
+    #chargement du dataframe
+    db=read_X_pages(folderName+"/"+dbName,1,nbPage)
+    dic = index_of(db,pageSize)
+    print(dic)
+    pages,lvl = index_page(dic)
+
+    for i,p in enumerate(pages):
+        T=p
+        T=pd.DataFrame(T,columns=['Y','Z'])
+        T.to_csv('Data/'+folderName+"_idx/"+"I_"+str(i)+".csv",sep=',',index=False)
+    return lvl
+
+def search_in_page(folderName,num_page,key,i,level,pageSize):
+    '''Renvoie la page du prochain niveau ou Z si le level est le dernier'''
+    cpt=0
+    with open('Data/'+folderName+"_idx/I_"+str(num_page)+".csv", 'r') as file:
+        spamreader = csv.reader(file, delimiter=',', quotechar='|')
+        a=next(spamreader)
+        #Tant que la key n'est pas supperieure et qu'il existe des lignes
+        for y in spamreader:
+            page=int(y[0])
+            if int(y[1])> key and level!=i or int(y[1])==key and level==i:
+                return page
+
+            
+        return page if level!=i else None
+       
+
+def search_index(folderName,level,key,pageSize,root=None):
+    '''Retourne le tuple si il existe dans l'index de la run folderName None sinon'''
+    lvl=1
+    l=0
+    #La racine est deja chargee en memoire
+    if root is not None:
+        while l<len(root.index) and int(root["Z"].get(l))<key:
+            l+=1
+        next_page=int(root["Y"].get(l-1))
+    else:
+        next_page=search_in_page(folderName,0,key,lvl,level,pageSize)
+    #on parcour les niveaux
+    while lvl<=level:
+        next_page=search_in_page(folderName,next_page,key,lvl,level,pageSize)
+        lvl+=1
+    return next_page
+        
+   
     
+    
+
+
+
+
+def main():
+    pass
     
   
   
@@ -159,11 +206,7 @@ def main():
             
 
 
- 
-  if B.search_key(47) is not None:
-    print("\nFound")
-  else:
-    print("\nNot Found")
+
  
  
 if __name__ == '__main__':
--- Codes/main.py
+++ Codes/main.py
@@ -2,31 +2,33 @@ from sortMerge import *
 from cartesian import *
 from tools import *
 from hash import *
+from index import *
 import time
 
 if __name__ == '__main__':
     
-    Rsize=300
+    Rsize=3000
     Ssize=10000
     selectivity=1
     memory=3
     pageSize=32
-    size_of_page=1024
-    size_of_tuple=32
-    size_key_index=8
     folderName="Run1"
 
 
     #Generation de données
 
-
     #R,S=generate_db(Rsize,Ssize,selectivity,double=False)
+    
+    lvl=index_to_file(folderName,pageSize,"S")
+    root=read_X_pages(folderName+"_idx/I",0,1)
+    print(root)
+    print(search_index(folderName,lvl,9000,pageSize))
     #db_to_file(R,pageSize,folderName,"R")
     #db_to_file(S,pageSize,folderName,"S")
     #seconds=time.time()
 
 
-    '''--------Test--------'''
+    '''--------Test--------
 
     #Théorique
 
@@ -36,7 +38,7 @@ if __name__ == '__main__':
     #Pratique
 
     #cartesian_product_file(folderName,memory,pageSize)
-    sort_merge_file(folderName,memory,pageSize)
+    #sort_merge_file(folderName,memory,pageSize)
 
 
 
@@ -51,67 +53,5 @@ if __name__ == '__main__':
     #test_sort_merge_join(Rsize,Ssize,selectivity,memory,size_of_tuple,size_of_page)
     #test_hybrid_hash_join(Rsize,Ssize,selectivity,memory,size_of_tuple,size_of_page)
     '''
-    R,S=generate_db(Rsize,Ssize,selectivity,double=False)
-    b,c=number_of_pages(R,32,1024)
-    print("Taille de R:")
-    print("Nombre de pages : ",b)
-    print("Index : ",c)
-
-    print("---------")
-    b,c=number_of_pages(S,32,1024,"B-arbre",32)
-    print("Taille de S:")
-    print("Nombre de pages : ",b)
-    print("Index : ",c)
     
-    ##############################
-    #sort-merge
-    ##############################
-    print("-"*10)
-    print("Sort-merge")
-    print("-"*10)
-    
-    A,r0,w0,r1,w1=sort_merge_join(R,S)
-    print(A)
-    print("----")
-    print("Pre-Traitement:\n")
-    print('Lecture :',r0,'/ Ecriture :',w0)
-    print("----")
-    print("Post-Traitement:\n")
-    print('Lecture :',r1,'/ Ecriture :',w1)
-    print("--")
-    print('Total : Lecture : ',r0+r1,' / Ecriture : ',w0+w1)
-    ##############################
-    #cartesien
-    ##############################
-    print("-"*10)
-    print("Cartesian")
-    print("-"*10)
-   
-    A,r0,w0,r1,w1=cartesian_product(R,S)
-    print(A)
-    print("----")
-    print("Pre-Traitement:\n")
-    print('Lecture :',0,'/ Ecriture :',0)
-    print("----")
-    print("Post-Traitement:\n")
-    print('Lecture :',r1,'/ Ecriture :',w1)
-    print("--")
-    print('Total : Lecture : ',r0+r1,' / Ecriture : ',w0+w1)
-
-    ##############################
-    #hachage
-    ##############################
-    print("-"*10)
-    print("Simple Hash")
-    print("-"*10)
-
-    A,r0,w0,r1,w1=hash_join(R,S)
-    print(A)
-    print("----")
-    print("Pre-Traitement:\n")
-    print('Lecture :',r0,'/ Ecriture :',w0)
-    print("----")
-    print("Post-Traitement:\n")
-    print('Lecture :',r1,'/ Ecriture :',w1)
-    print("--")
-    print('Total : Lecture : ',r0+r1,' / Ecriture : ',w0+w1)'''
+  
