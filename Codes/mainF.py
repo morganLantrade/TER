a5a23f77b006095539523ac5c7302c46c16b9fbe
Enguerran-Couderc <enguerrancouderc@gmail.com>
2023-07-25 04:42:19 +0200

    test time run update

--- Codes/mainF.py
+++ Codes/mainF.py
@@ -6,7 +6,7 @@ from index import *
 
 #Oral Mercredi 30 aout 13h30
 
-def time_test(RunName,folderName,pageSize,LMemory,repetition,MeanMin):
+def time_test_run(RunName,folderName,pageSize,LMemory,repetition):
 
     clean_data(folderName)
 
@@ -14,35 +14,51 @@ def time_test(RunName,folderName,pageSize,LMemory,repetition,MeanMin):
 
     seconds=time.time()
     nbOfTests=repetition*5*len(LMemory)
-    LTime=[]
+    LTimeMean=[]
+    LTimeMin=[]
+    LTimeMax=[]
     iTest=0
+
     for memory in LMemory:
-        LMean=[[] for _ in range(5)]
+        L=[[] for _ in range(5)]
         for i in range(repetition):
-            LMean[0].append(sort_merge_file(folderName,memory,pageSize))
+
+            L[0].append(sort_merge_file(folderName,memory,pageSize))
             iTest+=1
             percentage=round(((iTest/nbOfTests)*100))
             print("["+"■"*(percentage//2)+" "*(50-(percentage//2))+"] "+str(percentage)+"%", end="\r")
-            LMean[1].append(simple_hash_join_file(folderName,memory,pageSize))
+
+            L[1].append(simple_hash_join_file(folderName,memory,pageSize))
             iTest+=1
             percentage=round(((iTest/nbOfTests)*100))
             print("["+"■"*(percentage//2)+" "*(50-(percentage//2))+"] "+str(percentage)+"%", end="\r")
-            LMean[2].append(grace_hash_join_file(folderName,memory,pageSize))
+
+            L[2].append(grace_hash_join_file(folderName,memory,pageSize))
             iTest+=1
             percentage=round(((iTest/nbOfTests)*100))
             print("["+"■"*(percentage//2)+" "*(50-(percentage//2))+"] "+str(percentage)+"%", end="\r")
-            LMean[3].append(hybrid_hash_join_file(folderName,memory,pageSize))
+
+            L[3].append(hybrid_hash_join_file(folderName,memory,pageSize))
             iTest+=1
             percentage=round(((iTest/nbOfTests)*100))
             print("["+"■"*(percentage//2)+" "*(50-(percentage//2))+"] "+str(percentage)+"%", end="\r")
-            LMean[4].append(cartesian_product_index_file(folderName,memory,pageSize))
+
+            L[4].append(cartesian_product_index_file(folderName,memory,pageSize))
             iTest+=1
             percentage=round(((iTest/nbOfTests)*100))
             print("["+"■"*(percentage//2)+" "*(50-(percentage//2))+"] "+str(percentage)+"%", end="\r")
-        
-        LTime.append((memory,MeanMin(LMean[0]),MeanMin(LMean[1]),MeanMin(LMean[2]),MeanMin(LMean[3]),MeanMin(LMean[4])))
-    T=pd.DataFrame(LTime,columns=["Memory"]+LEGENDS[:-1])
-    T.to_csv('TimeTest/'+folderName+"_"+RunName+".csv",sep=',',index=False)
+
+        LTimeMean.append((memory,meanOfList(L[0]),meanOfList(L[1]),meanOfList(L[2]),meanOfList(L[3]),meanOfList(L[4])))
+        LTimeMin.append((memory,min(L[0]),min(L[1]),min(L[2]),min(L[3]),min(L[4])))
+        LTimeMax.append((memory,max(L[0]),max(L[1]),max(L[2]),max(L[3]),max(L[4])))
+
+    TMean=pd.DataFrame(LTimeMean,columns=["Memory"]+LEGENDS[:-1])
+    TMean.to_csv('TimeTest/'+folderName+"_"+RunName+"_mean.csv",sep=',',index=False)
+    TMin=pd.DataFrame(LTimeMin,columns=["Memory"]+LEGENDS[:-1])
+    TMin.to_csv('TimeTest/'+folderName+"_"+RunName+"_min.csv",sep=',',index=False)
+    TMax=pd.DataFrame(LTimeMax,columns=["Memory"]+LEGENDS[:-1])
+    TMax.to_csv('TimeTest/'+folderName+"_"+RunName+"_max.csv",sep=',',index=False)
+    clean_data(folderName)
     print("["+"■"*50+"] 100%")
     timer=round(time.time()-seconds)
     second=timer%60
@@ -52,16 +68,16 @@ def time_test(RunName,folderName,pageSize,LMemory,repetition,MeanMin):
 
 if __name__ == '__main__':
 
-    pageSize=256
+    pageSize=32
     Rsize=(11*pageSize)-5
     Ssize=Rsize*2
     selectivity=1
     memory=10
-    folderName="R11S22P256Sel1"
+    folderName="R11S22P32Sel1"
     #LMemory=[i for i in range (13,52,3)]+[i for i in range (52,103,5)]+[i for i in range (103,210,15)]+[210,211]
     LMemory=[i for i in range (5,11)]+[i for i in range (11,28,4)]
-    repetition=3
-    RunName="runpage256min"
+    repetition=5
+    RunName="petit_run"
     
     #test_result("R11S22P256Sel1","R11S22P256Sel1_cpi")
     
@@ -75,7 +91,7 @@ if __name__ == '__main__':
     #L=[]
     #--------Test--------
     
-    #time_test(RunName,folderName,pageSize,LMemory,repetition,min)
+    time_test_run(RunName,folderName,pageSize,LMemory,repetition)
 
     #Théorique
 
